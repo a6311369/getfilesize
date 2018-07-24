71bfe931d99417c1273346b99bdd67ebff8f562e
jiun <a6311369@gmail.com>
2018-07-24 22:11:17 +0800

    位元組轉換
    位元組轉換byte to MB ,GB

--- getfile2.py
+++ getfile2.py
@@ -1,27 +1,28 @@
 import os
 import os.path
-import math
+# import math
+
 list1 = []
 
 def get_size(path):
-    fileList = os.listdir(path)  # 获取path目录下所有文件
+    fileList = os.listdir(path)  # 獲取path目錄下所有文件
     for filename in fileList:
         # print(filename)         #印出檔案名稱
-        pathTmp = os.path.join(path,filename)  # 获取path与filename组合后的路径
-        if os.path.isdir(pathTmp):   # 判断是否为目录
+        pathTmp = os.path.join(path,filename)  # 獲取path與filename組合後的路徑
+        if os.path.isdir(pathTmp):   # 判斷是否為目錄
             get_size(pathTmp)        # 是目錄就繼續遞迴尋找
-        elif os.path.isfile(pathTmp):  # 判断是否为文件
-            filesize = os.path.getsize(pathTmp)  # 如果是文件，则获取相应文件的大小
-            filesize2 = int(filesize3(filesize))
-            print('檔案名稱：%s' % filename ,'檔案大小：%d byte' % filesize2 )
-            list1.append(filesize)      # 将文件的大小添加到列表
+        elif os.path.isfile(pathTmp):  # 判斷是否為檔案
+            filesize = filesize3(os.path.getsize(pathTmp))  # 如果是檔案，則獲取相應檔案的大小
+            # filesize2 = filesize3(filesize)
+            print('檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize )
+            list1.append(filesize)      # 將檔案的大小添加到列表
     # print(sum(list1))
     # print(fileList)
-# path= input("输入路径：").strip()  #由用户指定文件路径
+# path= input("输入路径：").strip()  #由用戶指定檔案路徑
 
 #位元組轉換
 def roundstr(size):
-    return str(round(size, 1))
+    return str(round(size, 1))   #round(size, 1) 取小數第一位後開始四捨五入
 def filesize3(bytesize):
     if bytesize < 1024:
         return str(bytesize) + ' Bytes'
