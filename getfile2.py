4aeef478b9d7e6bbf91721db85dfe0898de4b512
jiun <a6311369@gmail.com>
2018-07-27 00:13:06 +0800

    完成版
    檢查檔案判斷是否超過500MB，如果有就發送Mail通知

--- getfile2.py
+++ getfile2.py
@@ -7,31 +7,20 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE
 from getpass import getpass
 
-
-
 list1 = []
 b = []
 def get_size(path):
     fileList = os.listdir(path)  # 獲取path目錄下所有文件
     for filename in fileList:
-        # print(filename)         #印出檔案名稱
         pathTmp = os.path.join(path,filename)  # 獲取path與filename組合後的路徑
         if os.path.isdir(pathTmp):   # 判斷是否為目錄
             get_size(pathTmp)        # 是目錄就繼續遞迴尋找
         elif os.path.isfile(pathTmp):  # 判斷是否為檔案
             filesize9 = os.path.getsize(pathTmp)  # 如果是檔案，則獲取相應檔案的大小
             filesize = filesize3(os.path.getsize(pathTmp))  # 如果是檔案，則獲取相應檔案的大小
-            # filesize2 = filesize3(filesize)
-            if filesize9 > 536870912: #檔案大小比對，超過5MB就抓出來
+            if filesize9 > 536870912: #檔案大小比對，超過500MB就抓出來
                 a = '該檔案超過500MB','檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize
                 b.append(a) #將檔案名稱增加到list內
-                # print (a)
-                # print('該檔案超過1G','檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize)
-            # print('檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize )  # %s是接字串  %d是接數值
-            # list1.append(filesize)      # 將檔案的大小添加到列表
-    # print(sum(list1))
-    # print(fileList)
-# path= input("输入路径：").strip()  #由用戶指定檔案路徑
 
 #位元組轉換
 def roundstr(size):
@@ -52,10 +41,6 @@ def filesize3(bytesize):
 
 path = 'C:\\影片\\無聊詹'    #指定路徑
 get_size(path)
-# print('目錄中的文件總大小：%d Byte' % sum(list1))
-# for i in b:   #跑回圈輸出list內的資料
-#     print(i)
-
 
 # #send email
 OUTLOOK_USER = input("Enter your Outlook email: ")   #輸入outlook帳號
