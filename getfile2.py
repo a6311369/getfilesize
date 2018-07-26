4811ac883dcb7daac8179e09f240ba0e2cb0bcfa
jiun <a6311369@gmail.com>
2018-07-27 00:00:09 +0800

    偵測檔案並發送mail通知

--- getfile2.py
+++ getfile2.py
@@ -2,6 +2,11 @@
 import os
 import os.path
 import math
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE
+from getpass import getpass
+
 
 
 list1 = []
@@ -17,8 +22,8 @@ def get_size(path):
             filesize9 = os.path.getsize(pathTmp)  # 如果是檔案，則獲取相應檔案的大小
             filesize = filesize3(os.path.getsize(pathTmp))  # 如果是檔案，則獲取相應檔案的大小
             # filesize2 = filesize3(filesize)
-            if filesize9 > 1073741824: #檔案大小比對，超過1G就抓出來
-                a = '該檔案超過1G','檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize
+            if filesize9 > 536870912: #檔案大小比對，超過5MB就抓出來
+                a = '該檔案超過500MB','檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize
                 b.append(a) #將檔案名稱增加到list內
                 # print (a)
                 # print('該檔案超過1G','檔案名稱：%s' % filename ,'檔案大小：%s ' % filesize)
@@ -45,9 +50,42 @@ def filesize3(bytesize):
     else:
         return str(bytesize) + ' Bytes'
 
-path = 'E:\\ISO'    #指定路徑
+path = 'C:\\影片\\無聊詹'    #指定路徑
 get_size(path)
 # print('目錄中的文件總大小：%d Byte' % sum(list1))
-for i in b:   #跑回圈輸出list內的資料
-    print(i)
+# for i in b:   #跑回圈輸出list內的資料
+#     print(i)
 
+
+# #send email
+OUTLOOK_USER = input("Enter your Outlook email: ")   #輸入outlook帳號
+OUTLOOK_PWD = getpass("Enter password: ")            #輸入outlook密碼
+
+SUBJECT = 'Test Email' #mail主旨
+BODY = str(b)          #mail 內文
+TO = 'xxx@gmail.com'   #收件者mail
+
+def sendEmail(sender, pwd, to, subject, message):
+    recipient = to if type(to) is list else [to]
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = COMMASPACE.join(recipient)
+    server = smtplib.SMTP('smtp.live.com:587')  #hotmail的smtp server
+    server.ehlo()
+    server.starttls()
+
+    try:
+        server.login(sender,pwd)
+        print('Successfully authenticated...')
+    except smtplib.SMTPAuthenticationError:               # Check for authentication error
+        return " Authentication ERROR"
+
+    try:
+        server.sendmail(sender,recipient,msg.as_string())
+        print('Email sent!')
+    except smtplib.SMTPRecipientsRefused:                # Check if recipient's email was accepted by the server
+        return "ERROR"
+    server.quit()
+
+sendEmail(OUTLOOK_USER, OUTLOOK_PWD, TO, SUBJECT, BODY)
